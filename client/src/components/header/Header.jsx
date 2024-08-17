3351a5a096eab7d2d7485b9784cf072d4ee9aae0
Victoria Gopin <145561051+victoriagopin@users.noreply.github.com>
2024-08-17 16:55:56 +0300

    Create Header.jsx

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header(){
+    return (
+        
+    <div id="headerwrap">
+		<a href="" className="header main">CommunicateEasy</a>
+		<a href="" className="header">Profile</a>
+		<a href="" className="header">Register</a>
+		<a href="" className="header">Log In</a>
+		<a href="" className="header">Communicate Now</a>
+	</div>
+    )
+}
