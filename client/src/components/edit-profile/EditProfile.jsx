ba229d876f92afb93acf1f24f4975644497699e3
Victoria Gopin <145561051+victoriagopin@users.noreply.github.com>
2024-08-18 20:46:30 +0300

    Create EditProfile.jsx

---
+++ client/src/components/edit-profile/EditProfile.jsx
@@ -0,0 +1,40 @@
+export default function EditProfile(){
+    return (
+        <div className="container">
+		
+        <div className="col-lg-8 col-lg-offset-2 mt">
+            <form>
+                <h2 className="heading">Edit Your Profile</h2>
+                <div className="group">      
+                  <input type="text" required />
+                  <span className="highlight"></span>
+                  <span className="bar"></span>
+                  <label>Name</label>
+                </div>
+                  
+                <div className="group">      
+                  <input type="password" required />
+                  <span className="highlight"></span>
+                  <span className="bar"></span>
+                  <label>Occupation</label>
+                </div>
+
+                <div className="group">      
+                  <input type="password" required />
+                  <span className="highlight"></span>
+                  <span className="bar"></span>
+                  <label>About You</label>
+                </div>
+
+                <div className="group">      
+                  <input type="password" required />
+                  <span className="highlight"></span>
+                  <span className="bar"></span>
+                  <label>About You</label>
+                </div>
+                <p className="mt"><button type="button" className="btn btn-cta btn-lg">Edit</button></p>
+              </form>
+        </div>
+</div>
+    )
+}
