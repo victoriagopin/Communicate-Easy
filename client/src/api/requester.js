077b8670d228322918a399e4c9de9d0ad903ee4b
Victoria Gopin <145561051+victoriagopin@users.noreply.github.com>
2024-08-20 16:48:11 +0300

    Created requester to handle server requests

---
+++ client/src/api/requester.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const baseURL = 'http://localhost:5001';
+
+export async function requester(method, url, data){
+    const options = {
+        method,
+        headers: {},
+        url: `${baseURL}/${url}`,
+    }
+
+    if(data){
+        options.headers['Content-type'] = 'application/json';
+        options.data = data;
+    }
+
+    try{
+        const res = await axios(options);
+        
+        if(res.status == 200){
+            return res.data;
+        } else if (res == "null"){
+            return null;
+        }
+    } catch (error){
+        console.log(error.message);
+    }
+}
+
+export const get = (url, data) => requester('GET', url, data);
+export const post = (url, data) => requester('POST', url, data);
+export const put = (url, data) => requester('PUT', url, data);
+export const del = (url, data) => requester('DELETE', url, data);
