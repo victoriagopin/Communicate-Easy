83213c54e8ad22a209158cd9efb2249429d749b0
Victoria Gopin <145561051+victoriagopin@users.noreply.github.com>
2024-08-25 16:44:03 +0300

    Created routes for the chat requests

--- server/config/configRoutes.js
+++ server/config/configRoutes.js
@@ -1,9 +1,11 @@
 const {userRouter} = require('../routes/user')
-const {profileRouter} = require('../routes/profile')
+const {profileRouter} = require('../routes/profile');
+const { chatRouter } = require('../routes/chat');
 
 function configRoutes(app){
     app.use(userRouter);
     app.use(profileRouter);
+    app.use(chatRouter)
 }
 
 module.exports = {configRoutes}
---
+++ server/routes/chat.js
@@ -0,0 +1,46 @@
+const {Chat} = require('../models/Chat');
+const {Router} = require('express');
+
+const chatRouter = Router();
+
+chatRouter.get('/chat' ,async (req, res) => {
+    const participants = req.headers['x-participants']?.split(',');
+     
+    let chat = await Chat.findOne({participants:  participants});
+
+    res.json(chat);
+});
+
+chatRouter.post('/chat', async (req, res) => {
+    const { participants, sender, content } = req.body;
+    
+    let chat = await Chat.findOne({participants:  participants});
+
+    if(!chat){
+        console.log('no chat');
+        chat = new Chat({
+            participants,
+            messages: [{
+                sender, content,
+                timestamp: new Date(),
+                read : false
+            }],
+            lastUpdated: new Date()
+        })
+    } else {
+        const newMessage = {
+            sender, 
+            content,
+            timestamp : new Date(),
+            read: false
+        };
+        chat.messages.push(newMessage);
+        chat.lastUpdated = new Date();
+    }
+
+    await chat.save();
+
+    res.json(chat);
+})
+
+module.exports = {chatRouter} 
