a603d1bdb4590670a5db063c2214c09fbb70adc4
Victoria Gopin <145561051+victoriagopin@users.noreply.github.com>
2024-11-18 18:56:51 +0200

    Created hook to load all chats

---
+++ client/src/hooks/useGetChats.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { get } from "../api/requester";
+
+export function useGetChats(userId){
+    const [chats, setChats] = useState([]);
+    console.log(userId);
+
+    useEffect(() => {
+        const getChats = async () => {
+            const res = await get('my-chats', {} ,
+                {'x-userId': `${userId}`});
+
+                setChats(res);
+        }
+
+        getChats();
+    },[]);
+
+    return{
+        chats
+    }
+}
