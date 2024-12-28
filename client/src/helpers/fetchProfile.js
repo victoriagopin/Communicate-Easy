08640bd102fad3cb30a63daa7f2e6cff6698922d
Victoria Gopin <145561051+victoriagopin@users.noreply.github.com>
2024-12-28 16:37:13 +0200

    Update fetchProfile.js

--- client/src/helpers/fetchProfile.js
+++ client/src/helpers/fetchProfile.js
@@ -1,7 +1,6 @@
 import { get } from "../api/requester";
 
 export async function fetchProfile(id){
-    console.log(id);
     const profile = await get(`profile?owner=${id}`);
     const name = profile.fullName;
     return {name};
